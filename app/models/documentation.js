35ddb941f900b84bc43cd1c98f07a31b523ab378
Joël Gähwiler <joel.gaehwiler@gmail.com>
2016-08-23 10:34:59 +0200

    show madek data in admin

--- app/models/documentation.js
+++ app/models/documentation.js
@@ -3,6 +3,8 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   title: DS.attr('string'),
   slug: DS.attr('string'),
-  body: DS.attr('string'),
-  madekSet: DS.attr('string')
+  madekSet: DS.attr('string'),
+  videos: DS.attr(),
+  images: DS.attr(),
+  files: DS.attr()
 });
