e5a07274d30a9b9e8d67bc1843784ad68c9a4acd
Joël Gähwiler <joel.gaehwiler@gmail.com>
2016-09-13 16:30:07 +0200

    added basic password authentication for submit

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,8 +1,15 @@
+import Ember from 'ember';
 import DS from 'ember-data';
 import HasManyQuery from 'ember-data-has-many-query';
 
 import config from 'archive-app/config/environment';
 
 export default DS.JSONAPIAdapter.extend(HasManyQuery.RESTAdapterMixin, {
-  host: config.apiBaseURL + '/api'
+  session: Ember.inject.service(),
+  host: config.apiBaseURL + '/api',
+  headers: Ember.computed('session.password', function() {
+    return {
+      "Authorization": this.get('session.password'),
+    };
+  })
 });
