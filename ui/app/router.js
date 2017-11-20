d35d2c80bf7c3023bd0a87111ef4259888963c10
Joël Gähwiler <joel.gaehwiler@gmail.com>
2017-11-20 17:07:06 +0100

    added routes

--- ui/app/router.js
+++ ui/app/router.js
@@ -1,5 +1,5 @@
 import EmberRouter from '@ember/routing/router';
-import config from './config/environment';
+import config from 'archive/config/environment';
 
 const Router = EmberRouter.extend({
   location: config.locationType,
@@ -7,6 +7,31 @@ const Router = EmberRouter.extend({
 });
 
 Router.map(function() {
+  this.route('documentation', { path: 'd/:slug' });
+  this.route('person', { path: 'p/:slug' });
+  this.route('tag', { path: 't/:slug' });
+  this.route('year', { path: 'y/:year' });
+
+  this.route('submit');
+
+  this.route('admin', function(){
+    this.route('login');
+
+    this.route('documentations', function(){
+      this.route('new');
+      this.route('edit', { path: ':slug' });
+    });
+
+    this.route('people', function(){
+      this.route('new');
+      this.route('edit', { path: ':slug' });
+    });
+
+    this.route('tags', function(){
+      this.route('new');
+      this.route('edit', { path: ':slug' });
+    });
+  });
 });
 
 export default Router;
